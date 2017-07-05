d75357afc3b182214fbc3f293861c787efacfd93
Esti Shay and Kate Byars <kate@>
2017-07-05 14:36:07 -0700

    create initial project files and add text to letter.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var name = $("input#name").val();
+
+    $(".name").text(name);
+
+    $("#letter").show();
+
+    event.preventDefault();
+  });
+});
